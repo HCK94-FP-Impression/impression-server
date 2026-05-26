79f70527391dba2a1e720be4a5e67943a2ebb427
aaronarquette <aaronarquette538@outlook.com>
2026-05-26 12:12:10 +0700

    feat: add job recommendation endpoint  using Jooble api

--- routers/index.js
+++ routers/index.js
@@ -3,6 +3,7 @@ const authRouter = require("./authRouter");
 const ratingRouter = require("./ratingRouter");
 const postRouter = require("./postRouter");
 const cvRouter = require("./cvRouter");
+const jobRouter = require("./jobRouter");
 
 router.get("/", (req, res) => {
   res.send("Welcome to Impression API");
@@ -12,5 +13,6 @@ router.use("/auth", authRouter);
 router.use("/ratings", ratingRouter);
 router.use("/posts", postRouter);
 router.use("/cvs", cvRouter)
+router.use("/jobs", jobRouter)
 
 module.exports = router;
